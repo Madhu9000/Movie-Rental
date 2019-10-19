074cafe40c679f54c4599511c98f512f406fb138
Madhu9000 <podapati.madhunaidu@gmail.com>
2019-10-19 13:48:24 +0530

    Movie and Customer model classed added

---
+++ MovieMozo/MovieMozo/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="MovieMozo.MvcApplication" Language="C#" %>
---
+++ MovieMozo/MovieMozo/Models/Movie.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MovieMozo.Models
+{
+    public class Movie
+    {
+        public int Id { get; set; }
+        public string MovieName { get; set; }
+    }
+}
